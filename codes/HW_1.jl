ade78980b40758d5342d45726dd2af5b11908a84
Filipe Farias <filipepfarias@fisica.ufc.br>
2021-05-22 18:57:54 -0300

    Path fixed.

--- codes/HW_1.jl
+++ codes/HW_1.jl
@@ -1,21 +1,29 @@
 using DataFrames, CSV, Plots
 
-Income_df_sg = CSV.File("../data/ddf--gapminder--systema_globalis/countries-etc-datapoints/ddf--datapoints--income_per_person_gdppercapita_ppp_inflation_adjusted--by--geo--time.csv") |> DataFrame;
+Income_df_sg = CSV.File("./data/ddf--gapminder--systema_globalis/countries-etc-datapoints/ddf--datapoints--income_per_person_gdppercapita_ppp_inflation_adjusted--by--geo--time.csv") |> DataFrame;
 
-WorkingHours_df_sg = CSV.File("../data/ddf--gapminder--systema_globalis/countries-etc-datapoints/ddf--datapoints--working_hours_per_week--by--geo--time.csv") |> DataFrame;
+WorkingHours_df_sg = CSV.File("./data/ddf--gapminder--systema_globalis/countries-etc-datapoints/ddf--datapoints--working_hours_per_week--by--geo--time.csv") |> DataFrame;
 
-LiteracyRate_df_sg = CSV.File("../data/ddf--gapminder--systema_globalis/countries-etc-datapoints/ddf--datapoints--literacy_rate_adult_total_percent_of_people_ages_15_and_above--by--geo--time.csv") |> DataFrame;
+LiteracyRate_df_sg = CSV.File("./data/ddf--gapminder--systema_globalis/countries-etc-datapoints/ddf--datapoints--literacy_rate_adult_total_percent_of_people_ages_15_and_above--by--geo--time.csv") |> DataFrame;
 
-EnergyProd_df_sg = CSV.File("../data/ddf--gapminder--systema_globalis/countries-etc-datapoints/ddf--datapoints--energy_production_total--by--geo--time.csv") |> DataFrame;
+EnergyProd_df_sg = CSV.File("./data/ddf--gapminder--systema_globalis/countries-etc-datapoints/ddf--datapoints--energy_production_total--by--geo--time.csv") |> DataFrame;
 
-WaterWithdraw_df_sg = CSV.File("../data/ddf--gapminder--systema_globalis/countries-etc-datapoints/ddf--datapoints--water_withdrawal_cu_meters_per_person--by--geo--time.csv") |> DataFrame;
+WaterWithdraw_df_sg = CSV.File("./data/ddf--gapminder--systema_globalis/countries-etc-datapoints/ddf--datapoints--water_withdrawal_cu_meters_per_person--by--geo--time.csv") |> DataFrame;
 
-LifeExpectancy_df_sg = CSV.File("../data/ddf--gapminder--systema_globalis/countries-etc-datapoints/ddf--datapoints--life_expectancy_years--by--geo--time.csv") |> DataFrame;
+LifeExpectancy_df_sg = CSV.File("./data/ddf--gapminder--systema_globalis/countries-etc-datapoints/ddf--datapoints--life_expectancy_years--by--geo--time.csv") |> DataFrame;
 
-CivilSocPart_df_sg = CSV.File("../data/ddf--gapminder--fasttrack/ddf--datapoints--c_civsocpart_idea--by--country--time.csv") |> DataFrame;
+CivilSocPart_df_sg = CSV.File("./data/ddf--gapminder--fasttrack/ddf--datapoints--c_civsocpart_idea--by--country--time.csv") |> DataFrame;
 
-GDP_df_sg = CSV.File("../data/ddf--gapminder--systema_globalis/countries-etc-datapoints/ddf--datapoints--gdp_total_yearly_growth--by--geo--time.csv") |> DataFrame;
+GDP_df_sg = CSV.File("./data/ddf--gapminder--systema_globalis/countries-etc-datapoints/ddf--datapoints--gdp_total_yearly_growth--by--geo--time.csv") |> DataFrame;
 
-Gini_df_sg = CSV.File("../data/ddf--gapminder--systema_globalis/countries-etc-datapoints/ddf--datapoints--gapminder_gini--by--geo--time.csv") |> DataFrame;
+Gini_df_sg = CSV.File("./data/ddf--gapminder--systema_globalis/countries-etc-datapoints/ddf--datapoints--inequality_index_gini--by--geo--time.csv") |> DataFrame;
+
+function GetBrazilData(df_var,column)
+    return df_var[df_var[!,:geo].=="bra","$column"]
+end
 
 
+BRA_Gini = Gini_df_sg[Gini_df_sg[!,:geo].=="bra",:inequality_index_gini]
+BRA_Year = Gini_df_sg[Gini_df_sg[!,:geo].=="bra",:time]
+
+scatter(BRA_Year,BRA_Gini)
