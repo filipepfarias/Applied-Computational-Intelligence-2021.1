75c67bf558906a5739d89487b723122d6d0529cc
Yvo <yvojms@gmail.com>
2021-05-25 01:44:23 -0300

    Add correlation plot

--- hw1/codes/main.jl
+++ hw1/codes/main.jl
@@ -1,2 +1,9 @@
-using DataFrames, CSV, Plots, StatsBase, Missings, RDatasets
-air_quality_df = dropmissing(CSV.File("./data/AirQualityUCI.csv", delim=";") |> DataFrame);
+using DataFrames, CSV, Plots, StatsBase, Statistics
+
+air_quality_df = dropmissing(CSV.File("./data/AirQualityUCI.csv", delim=";", missingstring="-200") |> DataFrame)
+
+air_quality_matrix = Matrix{Real}(air_quality_df[:, 3:end])
+predictors_corr_matrix = cor(air_quality_matrix, air_quality_matrix)
+
+labels = Array(names(air_quality_df[:, 3:end]))
+Plots.heatmap(labels, labels, predictors_corr_matrix, size=(1500,1000))
