6ff60fe655bea552f17c7869d755a47a63858d87
Yvo <yvojms@gmail.com>
2021-05-25 11:13:32 -0300

    Update main.jl

--- hw1/codes/main.jl
+++ hw1/codes/main.jl
@@ -2,8 +2,10 @@ using DataFrames, CSV, Plots, StatsBase, Statistics
 
 air_quality_df = dropmissing(CSV.File("./data/AirQualityUCI.csv", delim=";", missingstring="-200") |> DataFrame)
 
-air_quality_matrix = Matrix{Real}(air_quality_df[:, 3:end])
+predictor_names = Array(["CO_GT", "PT08_S1_CO", "NMHC_GT", "C6H6_GT", "PT08_S2_NMHC", "NOx_GT", "PT08_S3_NOx", "NO2_GT", "PT08_S4_NO2", "PT08_S5_O3", "RH", "AH"])
+
+air_quality_matrix = Matrix{Real}(air_quality_df[:, predictor_names])
+
 predictors_corr_matrix = cor(air_quality_matrix, air_quality_matrix)
 
-labels = Array(names(air_quality_df[:, 3:end]))
-Plots.heatmap(labels, labels, predictors_corr_matrix, size=(1500,1000))
+Plots.heatmap(predictor_names, predictor_names, predictors_corr_matrix, size=(1500,1000))
