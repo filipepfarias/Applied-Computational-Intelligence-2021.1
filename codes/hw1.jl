62894d97ed75ad63becb64b61fff73f40bde94bf
Yvo <yvojms@gmail.com>
2021-05-27 12:54:50 -0300

    WIP

--- codes/hw1.jl
+++ codes/hw1.jl
@@ -7,7 +7,7 @@ using Statistics
 using LaTeXStrings
 using Latexify
 
-concrete_df = dropmissing(CSV.File("../data/ConcreteUCI.csv") |> DataFrame)
+concrete_df = dropmissing(CSV.File("./data/ConcreteUCI.csv") |> DataFrame)
 strength_categories = Array(["very low", "low", "medium", "high", "very high"])
 predictor_names = Array(["Cement", "Blast Furnace Slag", "Fly Ash", "Water", "Superplasticizer", "Coarse Aggregate", "Fine Aggregate", "Age (day)"])
 concrete_matrix = Matrix{Real}(concrete_df[:,predictor_names]);
@@ -16,6 +16,7 @@ num_predictors = length(predictor_names)
 num_categories = length(strength_categories)
 num_observations = nrow(concrete_df)
 
+latexify(L"N = %$num_observations") |> display
 latexify(L"D = %$num_predictors") |> display
 latexify(L"L = %$num_categories") |> display
 
@@ -32,23 +33,17 @@ for predictor_name in predictor_names
 end
 print(predictors_statistics_df)
 
-predictors_corr_matrix = cor(concrete_matrix, concrete_matrix)
-pyplot()
-f1 = heatmap(predictor_names, predictor_names, predictors_corr_matrix,xtickfontrotation=20,framestyle=:box,clim=(-1,1),color=:balance,aspect_ratio=:equal)
-savefig(f1,"../hw1/figures/predictors_corr_matrix.pdf");
+# predictors_corr_matrix = cor(concrete_matrix, concrete_matrix)
+# pyplot()
+# f1 = heatmap(predictor_names, predictor_names, predictors_corr_matrix,xtickfontrotation=20,framestyle=:box,clim=(-1,1),color=:balance,aspect_ratio=:equal)
+# savefig(f1,"./hw1/figures/predictors_corr_matrix.pdf");
 
-plot_matrix = Matrix{}(undef,num_predictors,num_predictors);
-
-for i in 1:num_predictors
-    for j in 1:num_predictors
-        if i == j
-            plot_matrix[i,j] = histogram(concrete_df[:,i])
-        else
-            plot_matrix[i,j] = scatter(concrete_df[:,i],concrete_df[:,j])
-        end
-    end    
+for category in 0:num_categories
+    f = plot_monovariate_histograms(concrete_df, predictor_names, category)
+    savefig(f,"./hw1/figures/monovariate_histograms_$category.pdf")
 end
 
-f2 = plot(plot_matrix[:]..., layout=(num_predictors,num_predictors), axis=false, ticks=false, legend=false)
-
-savefig(f2,"../hw1/figures/matrix-corplot.pdf");
+for category in 0:num_categories
+    f = plot_bivariate_scatters(concrete_df, predictor_names, category)
+    savefig(f,"./hw1/figures/bivariate_scatters_$category.pdf")
+end
