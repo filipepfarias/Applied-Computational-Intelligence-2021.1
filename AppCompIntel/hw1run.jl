2c40b438ec2244848979621f26a3385544e1762f
Yvo <yvojms@gmail.com>
2021-06-10 17:34:16 -0300

    Update hw1run.jl

--- AppCompIntel/hw1run.jl
+++ AppCompIntel/hw1run.jl
@@ -1,3 +1,4 @@
+using Base: Float64
 using Revise
 using AppCompIntel 
 using CSV
@@ -6,50 +7,73 @@ using PrettyTables
 using Statistics, StatsBase
 using Plots
 using Latexify, LaTeXStrings
+using YeoJohnsonTrans
+using BoxCoxTrans
+using MultivariateStats
 
 println("Running HW1 ...\n");
 println("Loading Concrete dataset...\n");
+pgfplotsx()
 
 concrete_df = CSV.File(eval(@__DIR__)*"/../data/Concrete_Data.csv") |> DataFrame;
+#filter!(col -> !any(value -> value == 0.0, col), concrete_df);
+transform!(concrete_df, "Concrete compressive strength (MPa)" => ByRow(strength -> get_category(strength)) => "Category");
 
-transform!(concrete_df, "Concrete compressive strength (MPa)" => ByRow(strength -> get_category(strength)) => "Category")
 
-open(eval(@__DIR__)*"/../data/ConcreteUCI.csv", "w") do file
-    CSV.write(file, concrete_df)
-end
+# open(eval(@__DIR__)*"/../data/ConcreteUCI.csv", "w") do file
+#     CSV.write(file, concrete_df)
+# end
 
-strength_categories = Array(["very low", "low", "medium", "high", "very high"]);
+strength_categories = Array(["not standard", "standard", "high strength"]);
 predictor_names = Array(["Cement", "Blast Furnace Slag", "Fly Ash", "Water", "Superplasticizer", "Coarse Aggregate", "Fine Aggregate", "Age (day)"]);
-concrete_matrix = Matrix{Real}(concrete_df[:,predictor_names]);
 
-display(concrete_df);
+concrete_matrix = Matrix{Float64}(concrete_df[:, predictor_names]);
+concrete_matrix = (concrete_matrix .- mean(concrete_matrix, dims=1)) ./ std(concrete_matrix, dims=1)
+#concrete_matrix_transf = mapslices(YeoJohnsonTrans.transform, concrete_matrix, dims=1)
 
-num_predictors   = length(predictor_names);
-num_categories   = length(strength_categories);
-num_observations = nrow(concrete_df);
+concrete_pca = MultivariateStats.fit(MultivariateStats.PCA, concrete_matrix', pratio=1)
+concrete_projected_matrix = MultivariateStats.transform(concrete_pca, concrete_matrix')
 
-predictors_statistics_df = DataFrame()
+# group results by category
+not_standard = concrete_projected_matrix[:, concrete_df[:, "Category"] .== 1]
+standard = concrete_projected_matrix[:, concrete_df[:, "Category"] .== 2]
+high_strength = concrete_projected_matrix[:, concrete_df[:, "Category"] .== 3]
 
-for predictor_name in predictor_names
-    predictor_array = Array(concrete_df[:, predictor_name])
-    append!(predictors_statistics_df, DataFrame(mean = mean(predictor_array), std = std(predictor_array), gamma = skewness(predictor_array)))
-end
-print(predictors_statistics_df)
+pca_scatter_plot = scatter(not_standard[1,:], not_standard[2,:], marker=:circle,linewidth=0, label="not standard")
+scatter!(standard[1,:], standard[2,:], marker=:circle, linewidth=0, label="standard")
+scatter!(high_strength[1,:], high_strength[2,:], marker=:circle, linewidth=0, label="high strength")
+plot!(pca_scatter_plot, xlabel="First principal component", ylabel="Second principal component", legendtitle="Concrete category", legend=:outertop, legendnrows=1)
 
-predictors_corr_matrix = cor(concrete_matrix, concrete_matrix)
+#save_if_isfile(pca_scatter_plot, "pca_scatter_plot.pdf")
+
+#save_if_isfile(plot(principalvars(concrete_pca), xlabel="Principal components", ylabel="Variance", legend = false), "pca_variance.pdf");
+# display(concrete_df);
+
+#num_predictors   = length(predictor_names);
+#num_categories   = length(strength_categories);
+#num_observations = nrow(concrete_df);
+
+# predictors_statistics_df = DataFrame()
+
+# for predictor_name in predictor_names
+#     predictor_array = Array(concrete_df[:, predictor_name])
+#     append!(predictors_statistics_df, DataFrame(mean = mean(predictor_array), std = std(predictor_array), gamma = skewness(predictor_array)))
+# end
+# print(predictors_statistics_df)
+
+# predictors_corr_matrix = cor(concrete_matrix, concrete_matrix)
 
 # pyplot()
 
 # f = heatmap(predictor_names, predictor_names, predictors_corr_matrix,xtickfontrotation=20,framestyle=:box,clim=(-1,1),color=:balance,aspect_ratio=:equal);
 # save_if_isfile(f,"predictors_corr_matrix.pdf");
 
-# gr()
 # for category in 0:num_categories
-#     f = plot_monovariate_histograms(concrete_df, predictor_names, category)
+#     f = plot_monovariate_histograms(concrete_df, predictor_names, category, false)
 #     save_if_isfile(f,"monovariate_histograms_$category.pdf");
 # end
 
-# for category in 0:num_categories
+# for category in 0:0
 #     f = plot_bivariate_scatters(concrete_df, predictor_names, category)
 #     save_if_isfile(f,"bivariate_scatters_$category.pdf");
 # end
